f2a87639390acf08bed9d10951623409c0be6336
Aman Agrawal <Aman_Agrawal@epam.com>
2024-10-01 18:26:58 +0530

    added some standard utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+// optional - to make it out from try catch block in controllers
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
